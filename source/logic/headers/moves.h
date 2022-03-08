932da4f8b53ef3509eb51bf4662320c373c9bbd4
tsmax2004 <tsoi.mv@phystech.edu>
2022-03-08 19:12:28 +0300

    grouped headers file in pieces.h and moves.h

---
+++ source/logic/headers/moves.h
@@ -0,0 +1,12 @@
+
+#ifndef MOVES_H_
+#define MOVES_H_
+
+#include "Moves/Move.h"
+#include "Moves/InvalidMove.h"
+#include "Moves/SimpleMove.h"
+#include "Moves/InPassing.h"
+#include "Moves/Promotion.h"
+#include "Moves/Castle.h"
+
+#endif
