a38fc0669d0c2579ad60b9aaba206473f34ec83a
evgen <ev.sklyarenko@gmail.com>
2022-03-10 23:53:52 +0300

    Corrected SimpleMove

--- source/logic/moves/SimpleMove.cpp
+++ source/logic/moves/SimpleMove.cpp
@@ -14,7 +14,31 @@ SimpleMove* SimpleMove::get_move() {
 }
 
 void SimpleMove::make_move(int from_row, int from_col, int to_row, int to_col, Position& position) const {
+    if (position.at(from_row, from_col)->piece_name_ == ROOK) {
+        if (position.at(from_row, from_col)->color_ == WHITE) {
+            if (from_row == 0 && from_col == 0) {
+                position.info_for_castle_[1] = true;
+            }
+            if (from_row == 0 && from_col == 7) {
+                position.info_for_castle_[2] = true;
+            }
+        }
+        if (position.at(from_row, from_col)->color_ == BLACK) {
+            if (from_row == 7 && from_col == 0) {
+                position.info_for_castle_[4] = true;
+            }
+            if (from_row == 7 && from_col == 7) {
+                position.info_for_castle_[5] = true;
+            }
+        }
+    }
+    if (position.at(from_row, from_col)->piece_name_ == KING) {
+        position.info_for_castle_[(position.at(from_row, from_col)->color_ == WHITE ? 0 : 3)];
+    }
+    position.move_color_ = (position.at(to_row, to_col)->color_ == WHITE ? BLACK : WHITE);
     position.board_.at(to_row).at(to_col) = position.board_.at(from_row).at(from_col);
-    position.board_.at(from_row).at(from_col)->piece_name_ = EMPTY;
-    position.board_.at(from_row).at(from_col)->color_ = WHITE;
+    position.at(from_row, from_col)->piece_name_ = EMPTY;
+    position.at(from_row, from_col)->color_ = WHITE;
+    position.last_move_ = {from_row, from_col, to_row, to_col};
+    position.position_type_ = NOT_DEFINE;
 };
