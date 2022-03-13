71d0161b25f6cec22db32ef5f7d8df456aaf9aa5
tsmax2004 <tsoi.mv@phystech.edu>
2022-03-13 14:35:19 +0300

    fixed Promotion.cpp

--- source/logic/moves/Promotion.cpp
+++ source/logic/moves/Promotion.cpp
@@ -12,8 +12,8 @@ Promotion* Promotion::get_move() {
 }
 
 void Promotion::make_move(int from_row, int from_col, int to_row, int to_col, Position& position) const {
-  position.board_[from_row][from_col] = Empty::get_piece();
   position.board_[to_row][to_col] = Queen::get_piece(position.at(from_row, from_col)->get_color());
+  position.board_[from_row][from_col] = Empty::get_piece();
   position.last_move_ = {from_row, from_col, to_row, to_col};
   position.move_color_ = {position.move_color_ == WHITE ? BLACK : WHITE};
 }
