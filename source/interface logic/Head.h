98f9ccb4bf570560157d9e0e904ae459399b6ac5
Supyness <ev.sklyarenko@gmail.com>
2022-04-10 02:06:16 +0300

    Head

--- source/interface logic/Head.h
+++ source/interface logic/Head.h
@@ -1,8 +1,18 @@
 #ifndef CHESSPROJECT_SOURCE_INTERFACE_LOGIC_HEAD_H_
 #define CHESSPROJECT_SOURCE_INTERFACE_LOGIC_HEAD_H_
 
-class Head {
+#include "include interface logic.h"
 
+
+class Head {
+  private:
+    std::shared_ptr<Interface> interface_;
+    std::shared_ptr<Game> game_;
+  public:
+    Head();
+    std::shared_ptr<Action> GetInterFaceAction();
+    void MenuCycle();
+    void GameCycle();
 };
 
 #endif //CHESSPROJECT_SOURCE_INTERFACE_LOGIC_HEAD_H_
