bc8b9774f4762b2dd9ea7ec6acdeb997d42828b3
Supyness <ev.sklyarenko@gmail.com>
2022-04-09 23:20:36 +0300

    CancelMove

--- source/interface logic/action/Action.h
+++ source/interface logic/action/Action.h
@@ -1,12 +1,13 @@
-//
-// Created by Георгий Кузнецов on 09.04.2022.
-//
-
+#include <string>
+#include "../../logic/headers/Game.h"
 #ifndef CHESSPROJECT_SOURCE_INTERFACE_LOGIC_ACTIONS_ACTION_H_
 #define CHESSPROJECT_SOURCE_INTERFACE_LOGIC_ACTIONS_ACTION_H_
 
 class Action {
-
+  private:
+    std::string type_;
+  public:
+    void virtual GetAction(Game*);
 };
 
 #endif //CHESSPROJECT_SOURCE_INTERFACE_LOGIC_ACTIONS_ACTION_H_
--- source/interface logic/action/actions types/CancelMove.h
+++ source/interface logic/action/actions types/CancelMove.h
@@ -1,12 +1,16 @@
-//
-// Created by Георгий Кузнецов on 09.04.2022.
-//
-
 #ifndef CHESSPROJECT_SOURCE_INTERFACE_LOGIC_ACTION_ACTIONS_TYPES_CANCELMOVE_H_
 #define CHESSPROJECT_SOURCE_INTERFACE_LOGIC_ACTION_ACTIONS_TYPES_CANCELMOVE_H_
+#include "../../logic/headers/Game.h"
+#include <string>
 
 class CancelMove {
-
+  private:
+    std::string type_ = "Cancel Move";
+    static std::shared_ptr<CancelMove> instance_;
+    CancelMove();
+  public:
+    static std::shared_ptr<CancelMove> GetInstance();
+    void GetAction(std::shared_ptr<Game>);
 };
 
 #endif //CHESSPROJECT_SOURCE_INTERFACE_LOGIC_ACTION_ACTIONS_TYPES_CANCELMOVE_H_
