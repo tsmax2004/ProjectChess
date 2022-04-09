3d8842a08f01c4d736240aa24446198a8fd80ebe
tsmax2004 <tsoi.mv@phystech.edu>
2022-04-09 12:22:12 +0300

    based functional of Main

---
+++ source/Main.h
@@ -0,0 +1,27 @@
+#ifndef CHESSPROJECT_SOURCE_MAIN_H_
+#define CHESSPROJECT_SOURCE_MAIN_H_
+
+#include "logic/headers/logic.h"
+
+class Main {
+ public:
+  Main();                                 // Создает Main, Interface, Game, вызывает Run()
+
+  void Run();                             // Запускает меню
+
+  void MakeMove(int, int, int, int);      // Метод, который вызывает интерфейс, если пользователь сделал ход
+  void CancelMove();                      // Метод, который вызывает интерфейс, если пользователь отменил послдений ход
+  void DeclareDraw();                     // Метод, который вызывает интерфейс, если пользователь объвил ничью
+  void DeclareGivingUp();                 // Метод. который вызывает интерфейс, если пользователь сдаётся
+
+  void Exit()                             // Выход из игры, удаляет интерфейс и логику
+
+ private:
+  void MenuCycle();                       // Менюшный цикл, отсюда можно выйти из игры, начать игру (т.е. войти в игровой цикл)
+  void GameCycle();                       // Игровой цикл, отсюда можно выйти в меню
+
+  Game game_logic_;
+  Interface interface_;
+};
+
+#endif //CHESSPROJECT_SOURCE_MAIN_H_
