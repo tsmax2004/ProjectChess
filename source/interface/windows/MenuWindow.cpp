b294cde765138186487a47b2799569f81d218535
mrnonam88 <91114975+mrnonam88@users.noreply.github.com>
2022-04-10 18:00:02 +0300

    Add Menu

--- source/interface/windows/MenuWindow.cpp
+++ source/interface/windows/MenuWindow.cpp
@@ -1,3 +1,22 @@
 #include "../headers/windows/MenuWindow.h"
-
+#include <iostream>
 MenuWindow::MenuWindow() = default;
+
+void MenuWindow::DrawMenuWindow() {
+  std::cout << "\nWelcome to ChessGame!\n";
+}
+
+MenuAction MenuWindow::GetAction() {
+  std::cout << '\n';
+  std::string response;
+  while ((response != "N") && (response != "E")) {
+    std::cout << "Options: (N)ew Game, (E)xitGame: ";
+    std::cin >> response;
+  }
+  if (response == "N") {
+    return MenuAction(MENU_ACTION_TYPE::NEW_GAME);
+  }
+  if (response == "E") {
+    return MenuAction(MENU_ACTION_TYPE::EXIT);
+  }
+}
